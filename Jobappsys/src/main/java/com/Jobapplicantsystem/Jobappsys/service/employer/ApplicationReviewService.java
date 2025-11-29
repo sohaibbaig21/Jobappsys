dd49e3f04e0d2ff1ab43ce742f017d5394b2bda5
sohaibbaig21 <sohaibbaig000@gmail.com>
2025-11-29 20:19:07 +0500

    Accidentally committed real Supabase database password and API keys. Now fixed it by repalcing Secrets with Variables and then filling the environment variables with real values

--- Jobappsys/src/main/java/com/Jobapplicantsystem/Jobappsys/service/employer/ApplicationReviewService.java
+++ Jobappsys/src/main/java/com/Jobapplicantsystem/Jobappsys/service/employer/ApplicationReviewService.java
@@ -3,8 +3,8 @@ package com.Jobapplicantsystem.Jobappsys.service.employer;
 import com.Jobapplicantsystem.Jobappsys.dto.response.ApplicationResponse;
 
 import java.util.List;
-
 public interface ApplicationReviewService {
+    
     void acceptApplication(Long applicationId);
     void rejectApplication(Long applicationId);
     List<ApplicationResponse> getApplicationsByJobPost(Long jobPostId);
